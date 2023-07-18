c07331a782f9436323838a3dc76060e6ecc5e11a
Adeline Merle <adeline.merle1@gmail.com>
2023-07-18 14:19:32 +0200

    attempt JS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,64 @@
 import "./style.scss";
 import Card from "./Card";
+import axios from "axios";
+import React, { useState } from "react";
+
+export interface Weather {
+  date: string;
+  icon: string;
+  temperature: number;
+}
 
 function App() {
+  // State
+  const [searchValue, setSearchValue] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+
+  const weathers: Weather[] = [
+    { date: "18/07", icon: "./src/assets/sun.png", temperature: 28 },
+    { date: "19/07", icon: "./src/assets/sun.png", temperature: 46 },
+    { date: "20/07", icon: "./src/assets/sun.png", temperature: 32 },
+  ];
+
+  // Comportement
+  const handleFormSubmit = async (e) => {
+    e.preventDefault(); // Empêche le comportement par défaut du formulaire
+
+    const enteredValue = e.target.elements.search.value;
+    setSearchValue(enteredValue);
+
+    try {
+      const response = await axios.get(
+        "https://api.openweathermap.org/data/2.5/forecast",
+        {
+          params: {
+            q: enteredValue,
+            appid: "00782a9b7d335a8491b0f238077e40d6c",
+            cnt: 5, // Récupérer les données météorologiques pour les 5 prochains jours
+          },
+        },
+      );
+
+      const weatherData = response.data.list.map((item) => ({
+        date: item.dt_txt,
+        icon: `https://openweathermap.org/img/w/${item.weather[0].icon}.png`,
+        temperature: item.main.temp,
+      }));
+
+      console.log(weatherData); // Affiche les données météorologiques extraites pour les 5 prochains jours
+    } catch (error) {
+      console.log("Error:", error);
+    }
+
+    e.target.elements.search.value = ""; // Réinitialise le champ de texte après la soumission
+  };
+
+  // Render
   return (
     <>
       <h1>Weather</h1>
-      <form id="form">
+      <form id="form" onSubmit={handleFormSubmit}>
         <input
           type="text"
           id="search"
@@ -17,16 +70,9 @@ function App() {
         </button>
       </form>
       <div className="cards">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {weathers.map((card) => (
+          <Card key={card.date} weather={card} />
+        ))}
       </div>
     </>
   );
--- src/Card.tsx
+++ src/Card.tsx
@@ -1,10 +1,10 @@
-function Card() {
+function Card({ weather }) {
   return (
     <article className="card">
-      <h2 className="card__date">18/07</h2>
-      <img className="card__picture" src="./src/assets/sun.png" alt="sun"></img>
+      <h2 className="card__date">{weather.date}</h2>
+      <img className="card__picture" src={weather.icon} alt="sun"></img>
       <div className="card__temperature">
-        <p>28&deg;</p>
+        <p>{weather.temperature}&deg;C</p>
       </div>
     </article>
   );
